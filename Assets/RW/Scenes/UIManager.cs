4be0c139604d683018a7ce92cb0876b54e58d449
Skylar Solewater <113034457+Amari768@users.noreply.github.com>
2022-12-07 07:00:33 -0500

    having difficulty with coding

--- Assets/RW/Scenes/UIManager.cs
+++ Assets/RW/Scenes/UIManager.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+public Animator startButton;
+public Animator settingsButton;
 public class UIManager : MonoBehaviour
+public Animator startButton;
+public Animator settingsButton;
 {
   public void StartGame() 
 {
     SceneManager.LoadScene("RocketMouse");
+}
+public void OpenSettings() 
+{
+    startButton.SetBool("isHidden", true);
+    settingsButton.SetBool("isHidden", true);
 }
 }
