3e1496b2ef42934852782dddb8534ee8e4684525
Skylar Solewater <113034457+Amari768@users.noreply.github.com>
2022-12-08 01:22:26 -0500

    erros persist

--- Assets/UIlayout/InventoryItemButton.cs
+++ Assets/UIlayout/InventoryItemButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Invintorybotton : MonoBehaviour
+public class InventoryItemButton : MonoBehaviour
 {
   private Text buttonText;
   private string[] itemTypes = {"Armor","Weapon","Spell"};
